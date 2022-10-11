ef297db87beafc7c12f62e544abcb56cb9959791
linhui <linhui@1cfun.com>
2022-10-11 17:55:27 +0800

    1

--- domain/user/model/Unit.php
+++ domain/user/model/Unit.php
@@ -9,5 +9,5 @@ class Unit{
     private $id;//int id
     private $name;//str
 }
-//与以往的仅有getter、setter的业务对象不同，领域对象具有了行为，对象更加丰满。同时，比起将这些逻辑写在服务内（例如**Service），领域功能的内聚性更强，职责更加明确。
+//与以往的仅有getter、setter的业务对象不同，领域对象具有了行为，对象更加丰满。同时，比起将这些逻辑写在服务内（例如**Service），领域功能的内聚性更强，职责更加明确。https://tech.meituan.com/2017/12/22/ddd-in-practice.html
 
